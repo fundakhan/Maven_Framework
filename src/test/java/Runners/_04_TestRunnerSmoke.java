f2d611913389d68f9ff8a3e64bfbf378811591b3
fundakhan <fundaakhan@gmail.com>
2022-10-19 15:02:24 -0400

    added plugin for Jenkins

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // hangi seneryolarda bu etiket varsa onlar calistirilacak
         features = {"src/test/java/FeatureFiles/"}, //hicbir class eklemedik hepsini otomatik alip calistirir
         glue = {"StepDefinitions"}
-      // , plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+       , plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} // Jenkins icin plugin ekledik
 
 )
 
--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@Regression or @SmokeTest", // hangi seneryolarda bu etiket varsa onlar calistirilacak
         features = {"src/test/java/FeatureFiles/"}, //hicbir class eklemedik hepsini otomatik alip calistirir
         glue = {"StepDefinitions"}
-
+        , plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} // jenkins icin plugin ekledik
 )
 
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
